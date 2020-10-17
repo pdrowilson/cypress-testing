9ce133e8c3e1f3d448788d8bc2893f049bfdbf8b
Pedro Wilson <pdro.wilson@gmail.com>
2020-10-16 22:17:55 -0300

    testing google search

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,11 +1,20 @@
 /// <reference types="cypress" />
 
 describe('test', () => {
-  it('test 01', () => {
+  it('testing google search', () => {
+    cy.visit('www.google.com');
+    cy.get('.gLFyf').type('youtube');
+    
+    // routing
+    // start server com cy.server()
+    // criar uma routa com cy.route()
+    // atribuir rota a um alias
+    
+    cy.server();
+    cy.route('GET', 'https://www.google.com/async/bgasy?ei=bEKKX4TxO7HY5OUPr6u4iAc&yv=3&async=_fmt:jspb').as('getYoutube');
+    
+    cy.get('.aajZCb > .tfB0Bf > center > .gNO89b').click();
 
-  });
-
-  it('test 02', () => {
-
+    cy.wait(2000)
   });
 });
